c98bb9fe9fa7b7ef30b5d8be58367f7219480f27
MatheusHenry2 <71864649+MatheusHenry2@users.noreply.github.com>
2022-04-23 22:34:23 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express();
+const cors = require('cors');
+
+const rotaEmpresa = require('./routes/empresa');
+const rotaUsuario = require('./routes/usuario');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors('*'));
+
+app.use("/empresa", rotaEmpresa)
+app.use("/usuario", rotaUsuario)
+
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, function() {
+    console.log("Server has started!")
+});
